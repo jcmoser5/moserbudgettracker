746a6399c4e85f86a9280e2b674eb08715d89104
Jonathan Moser <jonathancmoser@gmail.com>
2022-03-27 17:17:34 -0400

    add service worker

---
+++ public/service-worker.js
@@ -0,0 +1,65 @@
+const APP_PREFIX = 'BudgetTracker-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  '/js/index.js',
+  '/css/styles.css',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Files pre-cached successfully');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            console.log('Removing old cache data', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('fetch request : ' + e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(request) {
+      if (request) {
+        console.log(`responding with cache : ${e.request.url}`);
+        return request;
+      }
+      
+      console.log(`file is not cached, fetching : ${e.request.url}`);
+      fetch(e.request).then(response => {
+        if (response.status === 200) {
+          cache.put(e.request.url, response.clone());
+        }
+        return response;
+      });
+    })
+  );
+}); 
