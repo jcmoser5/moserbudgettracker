fe5313ff47416ab3b9f074a066d333ccacc5353b
Jonathan Moser <jonathancmoser@gmail.com>
2022-03-27 17:37:18 -0400

    updated formatting and service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,5 @@
-const APP_PREFIX = 'BudgetTracker-';
-const VERSION = 'version_01';
-const CACHE_NAME = APP_PREFIX + VERSION;
+const CACHE_NAME = 'Budget-Cache';
+const DATA_CACHE_NAME = 'Budget-Data-Cache';
 const FILES_TO_CACHE = [
   './',
   './index.html',
@@ -18,7 +17,7 @@ const FILES_TO_CACHE = [
   '/icons/icon-512x512.png'
 ];
 
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       console.log('Files pre-cached successfully');
@@ -26,15 +25,24 @@ self.addEventListener('install', function(e) {
     })
   );
 
-  self.skipWaiting();
+  e.waitUntil(
+    caches.open(DATA_CACHE_NAME).then(cache => {
+      return fetch('/api/transaction').then(response => {
+        if (response.status === 200) {
+          cache.put('api/transaction', response.clone());
+        }
+        return response;
+      });
+    })
+  );
 });
 
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', function (e) {
   e.waitUntil(
     caches.keys().then(keyList => {
       return Promise.all(
         keyList.map(key => {
-          if (key !== CACHE_NAME) {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
             console.log('Removing old cache data', key);
             return caches.delete(key);
           }
@@ -42,27 +50,35 @@ self.addEventListener('activate', function(e) {
       );
     })
   );
-
-  self.clients.claim();
 });
 
-self.addEventListener('fetch', function(e) {
-  console.log('fetch request : ' + e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function(request) {
-      if (request) {
-        console.log(`responding with cache : ${e.request.url}`);
-        return request;
-      }
-      
-      console.log(`file is not cached, fetching : ${e.request.url}`);
-      fetch(e.request).then(response => {
-        if (response.status === 200) {
-          caches.open(CACHE_NAME).then(cache => {
+self.addEventListener('fetch', function (e) {
+  if (e.request.url.includes('/api/') && e.request.method === 'GET') {
+    e.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(e.request).then(response => {
+          if (response.status === 200) {
             cache.put(e.request.url, response.clone());
+          }
+          return response;
+        })
+          .catch(err => {
+            return cache.match(e.request);
           });
+        })
+        .catch(err => console.log(err))
+    );
+    return;
+  }
+
+  e.respondWith(
+    fetch(e.request).catch(function () {
+      return caches.match(e.request).then(function (response) {
+        if (response) {
+          return response;
+        } else if (e.request.headers.get('accept').includes('text/html')) {
+          return caches.match('/');
         }
-        return response;
       });
     })
   );
